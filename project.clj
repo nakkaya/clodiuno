e72672a5ba3dd6e33dd4dfe91cdf8c4d1690c69f
Nurullah Akkaya <nurullah@nakkaya.com>
2010-04-05 11:26:21 +0300

    add project.clj and gitignore for lein.

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject clodiuno "0.0.1-SNAPSHOT"
+  :description "Clojure API for the firmata protocol."
+  :native-dependencies 
+  [[org.clojars.nakkaya/rxtx-macosx-native-deps "2.1.7"]]
+  :dev-dependencies [[native-deps "1.0.0"]
+                     [lein-clojars "0.5.0-SNAPSHOT"]])
