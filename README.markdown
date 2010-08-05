c7e68292ab8bccad124125e1d383ae1619f49e74
Nurullah Akkaya <nurullah@nakkaya.com>
2010-08-05 17:58:37 +0300

    update project.clj and README.markdown

--- README.markdown
+++ README.markdown
@@ -1,4 +1,4 @@
-Clodiuno is a Clojure API for the firmata protocol.
+Clojure API for Arduino.
 
 For installation instructions and examples check [project
 homepage](http://nakkaya.com/clodiuno.markdown).
--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
-(defproject clodiuno "0.0.1-SNAPSHOT"
-  :description "Clojure API for the firmata protocol."
-  :dependencies [[org.clojure/clojure "1.1.0"]
-		 [org.clojure/clojure-contrib "1.1.0"]]
+(defproject clodiuno "0.0.2-SNAPSHOT"
+  :description "Clojure API for Arduino."
+  :dependencies [[org.clojure/clojure "1.1.0"]]
   :native-dependencies 
   [[org.clojars.nakkaya/rxtx-macosx-native-deps "2.1.7"]]
   :dev-dependencies [[native-deps "1.0.0"]
