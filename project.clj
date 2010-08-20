2bc62a0622af34077d0a23a76c47e02c6b728ab8
Nurullah Akkaya <nurullah@nakkaya.com>
2010-08-20 21:22:33 +0300

    switch to 1.2.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clodiuno "0.0.2-SNAPSHOT"
   :description "Clojure API for Arduino."
-  :dependencies [[org.clojure/clojure "1.1.0"]]
+  :dependencies [[org.clojure/clojure "1.2.0"]]
   :native-dependencies 
   [[org.clojars.nakkaya/rxtx-macosx-native-deps "2.1.7"]]
   :dev-dependencies [[native-deps "1.0.0"]
