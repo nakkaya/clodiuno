77d0891536d05e1cab073c57538c2030cca31401
Nurullah Akkaya <nurullah@nakkaya.com>
2010-12-29 20:49:18 +0200

    update native-deps

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :dependencies [[org.clojure/clojure "1.2.0"]]
   :native-dependencies 
   [[org.clojars.nakkaya/rxtx-macosx-native-deps "2.1.7"]]
-  :dev-dependencies [[native-deps "1.0.4"]
+  :dev-dependencies [[native-deps "1.0.5"]
                      [lein-clojars "0.5.0-SNAPSHOT"]])
