7df14f89235755d6d00146621eb514b4c5b3d928
Nurullah Akkaya <nurullah@nakkaya.com>
2011-03-20 16:52:43 +0200

    update lein-clojars plugin.

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :native-dependencies 
   [[org.clojars.nakkaya/rxtx-macosx-native-deps "2.1.7"]]
   :dev-dependencies [[native-deps "1.0.5"]
-                     [lein-clojars "0.5.0-SNAPSHOT"]]
+                     [lein-clojars "0.6.0"]]
   :jvm-opts ["-Djava.library.path=./native/macosx/x86/"
              "-d32"])
