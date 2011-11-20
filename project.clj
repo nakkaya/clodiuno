fcbd3c4d3fb534baa7db90bc075edaf7938dcb37
Nurullah Akkaya <nurullah@nakkaya.com>
2011-11-21 01:36:43 +0200

    bump clojure version

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clodiuno "0.0.3-SNAPSHOT"
   :description "Clojure API for Arduino."
-  :dependencies [[org.clojure/clojure "1.2.0"]]
+  :dependencies [[org.clojure/clojure "1.3.0"]]
   :native-dependencies 
   [[org.clojars.nakkaya/rxtx-macosx-native-deps "2.1.7"]]
   :dev-dependencies [[native-deps "1.0.5"]
