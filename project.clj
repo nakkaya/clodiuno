e01841d36bea0f204dec341c8c8d86b2ac23c7bc
Nurullah Akkaya <nurullah@nakkaya.com>
2011-11-21 02:35:16 +0200

    added native deps for linux

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject clodiuno "0.0.3-SNAPSHOT"
   :description "Clojure API for Arduino."
   :dependencies [[org.clojure/clojure "1.3.0"]]
-  :native-dependencies 
-  [[org.clojars.nakkaya/rxtx-macosx-native-deps "2.1.7"]]
+  :native-dependencies [[org.clojars.nakkaya/rxtx-macosx-native-deps "2.1.7"]
+                        [org.clojars.mschristiansen/rxtx-linux-native-deps "2.1.7"]]
   :dev-dependencies [[native-deps "1.0.5"]
                      [lein-clojars "0.6.0"]]
-  :jvm-opts ["-Djava.library.path=./native/macosx/x86/"
+  :jvm-opts ["-Djava.library.path=./native/linux/x86/:./native/macosx/x86/"
              "-d32"])
