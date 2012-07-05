0b14efaa98bb5bd0c6eedafcbf778a8985de53a9
Nurullah Akkaya <nurullah@nakkaya.com>
2012-07-05 11:56:54 +0300

    bundle all rxtx deps

--- project.clj
+++ project.clj
@@ -1,8 +1,4 @@
 (defproject clodiuno "0.0.3-SNAPSHOT"
   :description "Clojure API for Arduino."
-  :dependencies [[org.clojure/clojure "1.3.0"]]
-  :native-dependencies [[org.clojars.nakkaya/rxtx-macosx-native-deps "2.1.7"]
-                        [org.clojars.mschristiansen/rxtx-linux-native-deps "2.1.7"]]
-  :dev-dependencies [[native-deps "1.0.5"]]
-  :jvm-opts ["-Djava.library.path=./native/linux/x86/:./native/macosx/x86/"
-             "-d32"])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [rxtx22 "1.0.6"]])
