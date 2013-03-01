0c83f12d374dcefd1484fec1469aec56a712ad33
Nurullah Akkaya <nurullah@nakkaya.com>
2013-03-01 23:15:35 +0200

    bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clodiuno "0.0.3-SNAPSHOT"
+(defproject clodiuno "0.0.4-SNAPSHOT"
   :description "Clojure API for Arduino."
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [rxtx22 "1.0.6"]])
